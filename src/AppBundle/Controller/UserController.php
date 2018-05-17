9825f1c48f3e9fe2c9224eca6613614b36f43944
tuyetrinhvo <you@example.com>
2018-05-17 20:30:32 +0200

    Create functions

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -13,6 +13,12 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\User;
+use AppBundle\Form\Type\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * Class UserController
@@ -20,10 +26,135 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @category PHP_Class
  * @package  AppBundle\Controller
  * @author   trinhvo <ttvdep@gmail.com>
- * @license  tuyetrinhvo@2018
+ * @license  License Name
  * @link     Link Name
  */
 class UserController extends Controller
 {
+    /**
+     * Function listAction
+     *
+     * @Route("/users", name="user_list")
+     * @Method({"GET"})
+     * @return          Response
+     */
+    public function listAction()
+    {
+        return $this->render(
+            'user/list.html.twig', [
+                'users' => $this->getDoctrine()
+                    ->getRepository('AppBundle:User')
+                    ->findAll()
+            ]
+        );
+    }
 
+    /**
+     * Function createAction
+     *
+     * @param Request $request Some argument description
+     *
+     * @Route("/users/create", name="user_create")
+     * @Method({"GET",         "POST"})
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function createAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render(
+            'user/create.html.twig', [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * Function editAction
+     *
+     * @param User    $user    Some argument description
+     * @param Request $request Some argument description
+     *
+     * @Route("/users/{id}/edit", name="user_edit")
+     * @Method({"GET",            "POST"})
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function editAction(User $user, Request $request)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render(
+            'user/edit.html.twig', [
+                'form' => $form->createView(), 'user' => $user
+            ]
+        );
+    }
+
+    /**
+     * Function deleteUserAction
+     *
+     * @param User $user Some argument description
+     *
+     * @Route("/users/{id}/delete", name="user_delete")
+     * @Method({"GET",              "POST"})
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteArticleAction(User $user)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+
+            $this->addFlash(
+                'error',
+                'Vous ne pouvez pas supprimer cet utilisateur car vous n\'êtes pas administrateur.'
+            );
+
+            return $this->redirectToRoute('user_list');
+
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'L\'utilisateur a bien été supprimé.');
+
+        return $this->redirectToRoute('user_list');
+
+    }
 }
