21b0b2759e4f0b1b677767ac4821489c96cc85a6
tuyetrinhvo <you@example.com>
2018-05-17 10:44:25 +0200

    update homepage

--- web/app_dev.php
+++ web/app_dev.php
@@ -37,7 +37,7 @@ $loader = include __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
